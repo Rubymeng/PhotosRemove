d348f067e3237fc96e8180bd52831ab67a99ff23
tyhmeng <tyhmeng@163,com>
2016-10-25 13:57:19 +0800

    Initial Commit

---
+++ PhotosRemove/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotosRemove
+//
+//  Created by tyhmeng on 16/10/25.
+//  Copyright © 2016年 tyhmeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhotosRemove/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotosRemove
+//
+//  Created by tyhmeng on 16/10/25.
+//  Copyright © 2016年 tyhmeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
